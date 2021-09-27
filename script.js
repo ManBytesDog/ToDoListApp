b0b7a8741387b50ed5a6e29309e97c6f7061ced7
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-09-27 19:27:20 -0400

    used localStorage.getItem to save the task.lists into local storage in browswer

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-let lists = []
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 
 newListForm.addEventListener('submit', event => {
 	event.preventDefault()

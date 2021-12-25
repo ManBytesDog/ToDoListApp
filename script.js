bdd26a3e5348a2cae7a545c30e1bad1a8dabcb4d
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-12-25 14:59:42 -0500

    added an event listner to 'clearCompletedTasksButton' to be able to delete completed tasks

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const tasksContainer = document.querySelector('[data-tasks]')
 const taskTemplate = document.getElementById('task-template')
 const newTaskForm = document.querySelector('[data-new-task-form]')
 const newTaskInput = document.querySelector('[data-new-task-input]')
+const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
@@ -30,6 +31,12 @@ tasksContainer.addEventListener('click', event => {
 		save()
 		renderTaskCount(selectedList)
 	}
+})
+
+clearCompleteTasksButton.addEventListener('click', event => {
+	const selectedList = lists.find(list => list.id === selectedListId)
+	selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
+	saveAndRender()
 })
 
 deleteListButton.addEventListener('click', event => {
